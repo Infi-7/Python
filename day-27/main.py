15db10c4e8dc9e4c7bc1687b5a470e2e1ae1caa7
Infi-7 <83002420+Infi-7@users.noreply.github.com>
2024-08-28 22:03:08 +0530

    commit 3

--- day-27/main.py
+++ day-27/main.py
@@ -1,12 +1,35 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "normal"))
-my_label.pack(side="left")
+my_label = Label(text="I am a label", font=("Arial", 24, "normal"))
+my_label.grid(row=0, column=0)
+
+# Button
+
+
+def button_clicked():
+    user_input = inp.get()
+
+    my_label["text"] = f"{str(user_input)}"
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.grid(row=1, column=1)
+
+# New Button
+
+new_button = Button(text="Click Me")
+new_button.grid(row=0, column=2)
+
+# Input
+
+inp = Entry()
+inp.grid(row=2, column=3)
+
 
 window.mainloop()
