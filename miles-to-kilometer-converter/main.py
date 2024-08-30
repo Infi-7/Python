b99946d7db559328ba76dc0b0da3272f5de021df
Infi-7 <bob@example.com>
2024-08-30 22:42:12 +0530

    addition of themes

--- miles-to-kilometer-converter/main.py
+++ miles-to-kilometer-converter/main.py
@@ -5,29 +5,42 @@ converter_win.title("Miles to Kilometer Converter")
 converter_win.minsize(width=300, height=200)
 converter_win.config(pady=20, padx=20, background="black")
 
+# theme changer
+choices = ["Light", "Dark"]
+variable = StringVar()
+variable.set(choices[0])
+
+theme_choice = OptionMenu(converter_win, variable, *choices)
+if theme_choice == "Dark":
+    print(variable)
+elif theme_choice == "Light":
+    print(variable)
+theme_choice.grid(row=0, column=6)
+
+
 # input
 user_input = Entry()
-user_input.grid(row=0, column=3)
+user_input.grid(row=1, column=3)
 
 # label 1
 label_1 = Label(text="Miles")
 label_1.config(padx=10, pady=10, background="black")
-label_1.grid(row=0, column=6)
+label_1.grid(row=1, column=6)
 
 # label 2
 label_2 = Label(text="is equal to")
 label_2.config(padx=10, pady=10, background="black", foreground="white")
-label_2.grid(row=1, column=0)
+label_2.grid(row=2, column=0)
 
 # label 3
 label_3 = Label(text="0")
 label_3.config(padx=10, pady=10, background="black", foreground="white")
-label_3.grid(row=1, column=3)
+label_3.grid(row=2, column=3)
 
 # label 4
 label_4 = Label(text="Km")
 label_4.config(padx=10, pady=10, background="black", foreground="white")
-label_4.grid(row=1, column=6)
+label_4.grid(row=2, column=6)
 
 
 # Button click function
@@ -39,7 +52,7 @@ def button_click():
 
 # Button
 calculate_button = Button(text="Calculate", command=button_click)
-calculate_button.grid(row=2, column=3)
+calculate_button.grid(row=3, column=3)
 calculate_button.config(padx=10, pady=10)
 
 converter_win.mainloop()
