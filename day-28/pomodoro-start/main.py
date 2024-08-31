a04b84c1ccc0da55c13a56ede49dc6605aa23cf3
Infi-7 <bob@example.com>
2024-08-31 20:09:12 +0530

    UI implementation

--- day-28/pomodoro-start/main.py
+++ day-28/pomodoro-start/main.py
@@ -29,6 +29,22 @@ canvas = Canvas(width=205, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=img)
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+# label 1
+timer_label = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(row=0, column=1)
+
+# Start Button
+start = Button(text="Start")
+start.grid(row=3, column=0)
+
+# Reset Button
+reset = Button(text="Reset")
+reset.grid(row=3, column=3)
+
+# Label 2
+check_label = Label(text="✔", background=YELLOW, foreground=GREEN, font=(FONT_NAME, 20, "bold"))
+check_label.grid(row=4, column=1)
 
 window.mainloop()
