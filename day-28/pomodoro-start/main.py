ff88c6119183139a6f3c2943b0a03fa4603a4aff
Infi-7 <bob@example.com>
2024-08-31 22:28:32 +0530

    timer logic update

--- day-28/pomodoro-start/main.py
+++ day-28/pomodoro-start/main.py
@@ -1,4 +1,6 @@
 # imports
+import math
+import time
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -7,40 +9,69 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 25 * 60
+SHORT_BREAK_MIN = 5 * 60
+LONG_BREAK_MIN = 20 * 60
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        count_down(10)
+        status_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(5)
+        status_label.config(text="Break", fg=PINK)
+    else:
+        count_down(20)
+        status_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_temp = count
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec == 0 or count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 # Window
 window = Tk()
 window.title("Pomodoro")
 window.config(pady=50, padx=100, bg=YELLOW)
 
+
 # Canvas
 canvas = Canvas(width=205, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=img)
-canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 # label 1
-timer_label = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 35, "bold"))
-timer_label.grid(row=0, column=1)
+status_label = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 35, "bold"))
+status_label.grid(row=0, column=1)
 
 # Start Button
-start = Button(text="Start")
+start = Button(text="Start", highlightthickness=0, command=start_timer)
 start.grid(row=3, column=0)
 
 # Reset Button
-reset = Button(text="Reset")
+reset = Button(text="Reset", highlightthickness=0)
 reset.grid(row=3, column=3)
 
 # Label 2
