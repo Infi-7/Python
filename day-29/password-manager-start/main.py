5d3e23d79519f61f1fc418d9e041662d84f4901d
aniket209 <patilaniket209@gmail.com>
2024-09-03 01:03:18 +0530

    UI additions

--- day-29/password-manager-start/main.py
+++ day-29/password-manager-start/main.py
@@ -20,7 +20,38 @@ window.config(pady=PADDING, padx=PADDING)
 canvas = Canvas(height=HEIGHT, width=WIDTH)
 img = PhotoImage(file="logo.png")
 canvas.create_image(WIDTH/2, HEIGHT/2, image=img)
+canvas.grid(row=0, column=1)
 
-canvas.grid(row=0, column=0)
+# Website Label
+label_website = Label(text="Website: ")
+label_website.grid(row=1, column=0)
+
+# Username/Email Label
+label_user_email = Label(text="Email/Username: ")
+label_user_email.grid(row=2, column=0)
+
+# Password Label
+label_password = Label(text="Password: ")
+label_password.grid(row=3, column=0)
+
+# Website Textbox
+text_website = Entry(width=35)
+text_website.grid(row=1, column=1)
+
+# Username/Email Textbox
+text_user_email = Entry(width=35)
+text_user_email.grid(row=2, column=1)
+
+# Password Textbox
+text_password = Entry(width=21)
+text_password.grid(row=3, column=1)
+
+# Add button
+button_add = Button(text="Add", width=35)
+button_add.grid(row=4, column=1)
+
+# Generate Button
+button_generate_password = Button(text="Generate Password")
+button_generate_password.grid(row=3, column=2)
 
 window.mainloop()
