c977cbc96e05df96e9c8a64049e104d8a71da250
aniket209 <patilaniket209@gmail.com>
2024-09-03 22:57:46 +0530

    UI design final update

--- day-29/password-manager-start/main.py
+++ day-29/password-manager-start/main.py
@@ -4,7 +4,7 @@ from tkinter import *
 # variables
 WIDTH = 200
 HEIGHT = 200
-PADDING = 20
+PADDING = 50
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -19,39 +19,39 @@ window.config(pady=PADDING, padx=PADDING)
 # canvas
 canvas = Canvas(height=HEIGHT, width=WIDTH)
 img = PhotoImage(file="logo.png")
-canvas.create_image(WIDTH/2, HEIGHT/2, image=img)
+canvas.create_image(65, HEIGHT/2, image=img)
 canvas.grid(row=0, column=1)
 
 # Website Label
 label_website = Label(text="Website: ")
-label_website.grid(row=1, column=0)
+label_website.grid(row=1, column=0, padx=PADDING - 47, pady=PADDING - 47)
 
 # Username/Email Label
 label_user_email = Label(text="Email/Username: ")
-label_user_email.grid(row=2, column=0)
+label_user_email.grid(row=2, column=0, padx=PADDING - 47, pady=PADDING - 47)
 
 # Password Label
 label_password = Label(text="Password: ")
-label_password.grid(row=3, column=0)
+label_password.grid(row=3, column=0, padx=PADDING - 47, pady=PADDING - 47)
 
 # Website Textbox
-text_website = Entry(width=35)
-text_website.grid(row=1, column=1)
+text_website = Entry(width=40)
+text_website.grid(row=1, column=1, columnspan=2, stick="W")
 
 # Username/Email Textbox
-text_user_email = Entry(width=35)
-text_user_email.grid(row=2, column=1)
+text_user_email = Entry(width=40)
+text_user_email.grid(row=2, column=1, columnspan=2, stick="W")
 
 # Password Textbox
 text_password = Entry(width=21)
-text_password.grid(row=3, column=1)
+text_password.grid(row=3, column=1, columnspan=2, stick="W")
 
 # Add button
-button_add = Button(text="Add", width=35)
-button_add.grid(row=4, column=1)
+button_add = Button(text="Add", width=33)
+button_add.grid(row=4, column=1, padx=PADDING - 47, pady=PADDING - 47, columnspan=2, sticky="W")
 
 # Generate Button
 button_generate_password = Button(text="Generate Password")
-button_generate_password.grid(row=3, column=2)
+button_generate_password.grid(row=3, column=1, stick="E",columnspan=2)
 
 window.mainloop()
