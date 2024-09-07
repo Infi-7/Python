2a520662c3a2d039a8f4a4fc24326d0877413e74
Infi-7 <bob@example.com>
2024-09-07 17:35:57 +0530

    correction to UI update

--- day-31/flash-card-project-start/main.py
+++ day-31/flash-card-project-start/main.py
@@ -8,28 +8,27 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Flashy")
 window.minsize(width=200, height=200)
-window.config(bg=BACKGROUND_COLOR, padx=50)
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
-# Canvas Settings
-canvas = Canvas(width=800, height=530, bg=BACKGROUND_COLOR, highlightthickness=0)
+# Main Canvas Settings
+canvas_main = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_back = PhotoImage(file="./images/card_back.png")
 card_front = PhotoImage(file="./images/card_front.png")
 guess_check = PhotoImage(file="./images/right.png")
 guess_wrong = PhotoImage(file="./images/wrong.png")
 
-canvas.create_image(400, 265, image= card_back)
-canvas.grid(row=0, column=0,columnspan=2)
-
-canvas.create_image(400, 265, image= card_front)
-canvas.grid(row=0, column=1)
+canvas_main.create_image(400, 265, image=card_front)
+canvas_main.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas_main.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas_main.grid(row=0, column=0, columnspan=2)
 
 # Wrong Button
 button_wrong = Button(image=guess_wrong, highlightthickness=0)
-button_wrong.grid(row=1, column=0, sticky="w",columnspan=2)
+button_wrong.grid(row=1, column=0)
 
 # Check Button
-check_wrong = Button(image=guess_check, highlightthickness=0)
-check_wrong.grid(row=1, column=1, sticky="e")
+button_check = Button(image=guess_check, highlightthickness=0)
+button_check.grid(row=1, column=1)
 
 
-mainloop()
+window.mainloop()
