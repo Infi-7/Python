f02fb070c9dc60511b288e3434adf3419fb3f0bb
Infi-7 <bob@example.com>
2024-09-07 20:21:22 +0530

    both working buttons feature

--- day-31/flash-card-project-start/main.py
+++ day-31/flash-card-project-start/main.py
@@ -1,9 +1,45 @@
+import random
 from tkinter import *
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
+QUESTION = ""
+ANSWER = ""
+correct = []
+skipped = []
+RNG = 0
+
+
+# Main code
+og_df = pd.read_csv("./data/french_words.csv")
+df = og_df.to_dict()
+
+
+def word_display():
+    global RNG, QUESTION, ANSWER
+    RNG = 0
+    QUESTION = ""
+    ANSWER = ""
+    RNG = random.randint(0, len(og_df) - 1)
+    if len(correct) == 0 or RNG not in correct or RNG in skipped:
+        QUESTION = df["French"][RNG]
+        ANSWER = df["English"][RNG]
+        canvas_main.itemconfig(replace_words, text=QUESTION)
+    else:
+        word_display()
+
+
+def correct_guess():
+    word_display()
+    correct.append(RNG)
+
+
+def skipped_word():
+    skipped.append(RNG)
+    word_display()
+
 
 # UI
-
 # Window Settings
 window = Tk()
 window.title("Flashy")
@@ -18,17 +54,17 @@ guess_check = PhotoImage(file="./images/right.png")
 guess_wrong = PhotoImage(file="./images/wrong.png")
 
 canvas_main.create_image(400, 265, image=card_front)
-canvas_main.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas_main.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas_main.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+replace_words = canvas_main.create_text(400, 263, text=f"{QUESTION}", font=("Ariel", 60, "bold"))
+word_display()
 canvas_main.grid(row=0, column=0, columnspan=2)
 
 # Wrong Button
-button_wrong = Button(image=guess_wrong, highlightthickness=0)
+button_wrong = Button(image=guess_wrong, highlightthickness=0, command=skipped_word)
 button_wrong.grid(row=1, column=0)
 
 # Check Button
-button_check = Button(image=guess_check, highlightthickness=0)
+button_check = Button(image=guess_check, highlightthickness=0, command=correct_guess)
 button_check.grid(row=1, column=1)
-
 
 window.mainloop()
