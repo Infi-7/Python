3c3d623a748bb9fd2c326cb9aaab7d22b74af0ee
Infi-7 <bob@example.com>
2024-09-07 21:46:55 +0530

    minor updates

--- day-31/flash-card-project-start/main.py
+++ day-31/flash-card-project-start/main.py
@@ -9,24 +9,32 @@ correct = []
 skipped = []
 RNG = 0
 
-
 # Main code
 og_df = pd.read_csv("./data/french_words.csv")
 df = og_df.to_dict()
 
 
 def word_display():
-    global RNG, QUESTION, ANSWER
+    global RNG, QUESTION, ANSWER, flip_timer
     RNG = 0
     QUESTION = ""
     ANSWER = ""
     RNG = random.randint(0, len(og_df) - 1)
+
+    canvas_main.itemconfig(image_display, image=card_front)
     if len(correct) == 0 or RNG not in correct or RNG in skipped:
+        window.after_cancel(flip_timer)
         QUESTION = df["French"][RNG]
         ANSWER = df["English"][RNG]
-        canvas_main.itemconfig(replace_words, text=QUESTION)
-    else:
-        word_display()
+        canvas_main.itemconfig(replace_language, text="French", fill="black")
+        canvas_main.itemconfig(replace_words, text=QUESTION, fill="black")
+        flip_timer = window.after(3000, func=display_translated)
+
+
+def display_translated():
+    canvas_main.itemconfig(replace_language, text="English", fill="white")
+    canvas_main.itemconfig(replace_words, text=ANSWER, fill="white")
+    canvas_main.itemconfig(image_display, image=card_back)
 
 
 def correct_guess():
@@ -45,6 +53,7 @@ window = Tk()
 window.title("Flashy")
 window.minsize(width=200, height=200)
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+flip_timer = window.after(3000, func=display_translated)
 
 # Main Canvas Settings
 canvas_main = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -53,8 +62,8 @@ card_front = PhotoImage(file="./images/card_front.png")
 guess_check = PhotoImage(file="./images/right.png")
 guess_wrong = PhotoImage(file="./images/wrong.png")
 
-canvas_main.create_image(400, 265, image=card_front)
-canvas_main.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+image_display = canvas_main.create_image(400, 265, image=card_front)
+replace_language = canvas_main.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
 replace_words = canvas_main.create_text(400, 263, text=f"{QUESTION}", font=("Ariel", 60, "bold"))
 word_display()
 canvas_main.grid(row=0, column=0, columnspan=2)
