3b63111f0b3d85271ca05b3095038e3b65ae0147
Infi-7 <bob@example.com>
2024-09-08 20:36:47 +0530

    final commit

--- day-31/flash-card-project-start/main.py
+++ day-31/flash-card-project-start/main.py
@@ -1,79 +1,71 @@
-import random
 from tkinter import *
-import pandas as pd
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-QUESTION = ""
-ANSWER = ""
-correct = []
-skipped = []
-RNG = 0
+current_card = {}
+to_learn = {}
 
-# Main code
-og_df = pd.read_csv("./data/french_words.csv")
-df = og_df.to_dict()
-
-
-def word_display():
-    global RNG, QUESTION, ANSWER, flip_timer
-    RNG = 0
-    QUESTION = ""
-    ANSWER = ""
-    RNG = random.randint(0, len(og_df) - 1)
-
-    canvas_main.itemconfig(image_display, image=card_front)
-    if len(correct) == 0 or RNG not in correct or RNG in skipped:
-        window.after_cancel(flip_timer)
-        QUESTION = df["French"][RNG]
-        ANSWER = df["English"][RNG]
-        canvas_main.itemconfig(replace_language, text="French", fill="black")
-        canvas_main.itemconfig(replace_words, text=QUESTION, fill="black")
-        flip_timer = window.after(3000, func=display_translated)
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-def display_translated():
-    canvas_main.itemconfig(replace_language, text="English", fill="white")
-    canvas_main.itemconfig(replace_words, text=ANSWER, fill="white")
-    canvas_main.itemconfig(image_display, image=card_back)
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-def correct_guess():
-    word_display()
-    correct.append(RNG)
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
-def skipped_word():
-    skipped.append(RNG)
-    word_display()
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
-# UI
-# Window Settings
 window = Tk()
 window.title("Flashy")
-window.minsize(width=200, height=200)
-window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
-flip_timer = window.after(3000, func=display_translated)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# Main Canvas Settings
-canvas_main = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_back = PhotoImage(file="./images/card_back.png")
-card_front = PhotoImage(file="./images/card_front.png")
-guess_check = PhotoImage(file="./images/right.png")
-guess_wrong = PhotoImage(file="./images/wrong.png")
+flip_timer = window.after(3000, func=flip_card)
 
-image_display = canvas_main.create_image(400, 265, image=card_front)
-replace_language = canvas_main.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-replace_words = canvas_main.create_text(400, 263, text=f"{QUESTION}", font=("Ariel", 60, "bold"))
-word_display()
-canvas_main.grid(row=0, column=0, columnspan=2)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-# Wrong Button
-button_wrong = Button(image=guess_wrong, highlightthickness=0, command=skipped_word)
-button_wrong.grid(row=1, column=0)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-# Check Button
-button_check = Button(image=guess_check, highlightthickness=0, command=correct_guess)
-button_check.grid(row=1, column=1)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
+
+
+
