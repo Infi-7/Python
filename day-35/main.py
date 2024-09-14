56ca3020035b19d457684b7553c17b792d82ffc4
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-09-14 18:32:25 +0530

    sms sender implementation

--- day-35/main.py
+++ day-35/main.py
@@ -1,10 +1,15 @@
 import requests
+from twilio.rest import Client
 
 KEY = "2b877cfbea9f8de6577978ccdb62f0ba"
 LATITUDE = 19.393345
 LONGITUDE = 72.862427
 URL = "https://api.openweathermap.org/data/2.5/forecast"
 CNT = 4
+account_sid = "ACc145f1feaf6961edc7fddce6fa276666"
+auth_token = "4c92718940e917ade54ade0664e5b6a4"
+
+
 weather_params = {
     "lat": LATITUDE,
     "lon": LONGITUDE,
@@ -24,5 +29,12 @@ for x in range(0, CNT):
 
 for x in id_entries:
     if x < 700:
-        print("Bring an Umbrella")
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body="It's going to rain today. Remember to bring an ☔",
+            from_="+12073557430",  # generated
+            to="+917720834048",  # real
+        )
+
+        print(message.status)
         break
