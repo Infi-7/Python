0630de0193f407fce0cc49477bcb62b8f216a160
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-09-27 20:13:23 +0530

    selenium introduction

---
+++ day-48/main.py
@@ -0,0 +1,30 @@
+from re import search
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(options=chrome_options)
+'''
+driver.get("https://www.amazon.com/dp/B01NBKTPTS/ref=twister_B0DHW6NSX1?_encoding=UTF8&th=1")
+
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction").text
+
+print(f"{price_dollar}.{price_cents}")'''
+
+driver.get("https://www.python.org/")
+
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.get_attribute("placeholder"))
+
+button = driver.find_element(By.ID, value="submit")
+print(button.size)
+
+documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+print(documentation_link.text )
+
+# driver.close()
+driver.quit()
