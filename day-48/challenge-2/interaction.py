dfbda11167d593d290e05cefbaa5a2472407dd95
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-09-29 18:01:34 +0530

    challenge 2 and 3 complete update

--- day-48/challenge-2/interaction.py
+++ day-48/challenge-2/interaction.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
@@ -11,8 +11,15 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 # article_count = driver.find_element(By.CSS_SELECTOR, value="a[title='Special:Statistics']").text
 # print(article_count)
 
-all_portals = driver.find_element(By.LINK_TEXT,value="Content portals")
-all_portals.click()
+# all_portals = driver.find_element(By.LINK_TEXT,value="Content portals")
+# all_portals.click()
+
+search_button = driver.find_element(By.CSS_SELECTOR,value="a[href='/wiki/Special:Search']")
+search_button.click()
+
+search = driver.find_element(By.NAME,value="search")
+search.send_keys("Python", Keys.ENTER)
+
 
 
 # driver.quit()
