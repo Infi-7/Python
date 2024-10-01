f61199b844a0f528c7c465af48c702293d7cb5c9
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-10-01 21:50:03 +0530

    full form automation incomplete

--- day-53/data-entry-job-automation/main.py
+++ day-53/data-entry-job-automation/main.py
@@ -1,6 +1,5 @@
 import os
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
@@ -10,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SHEETS_LINK = os.getenv("sheets_link")
+SHEETS_LINK2 = os.getenv("sheets_link2")
 print(SHEETS_LINK)
 
 response = requests.get("https://appbrewery.github.io/Zillow-Clone/").text
@@ -37,12 +37,38 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(SHEETS_LINK)
 
-address = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
-price = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
-links = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
-submit_clicker = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div")
+driver2 = webdriver.Chrome(options=chrome_options)
+driver2.get(SHEETS_LINK2)
 
-address.send_keys(prop_address[0])
-price.send_keys(prop_price[0])
-links.send_keys(prop_links[0])
-submit_clicker.click()
+minor_address = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+minor_price = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+minor_links = driver.find_element(By.XPATH,"/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+minor_submit_clicker = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div")
+
+
+
+
+major_address = driver2.find_element(By.XPATH,"//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+major_price = driver2.find_element(By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+major_links = driver2.find_element(By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+major_submit_clicker = driver2.find_element(By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div")
+
+
+for entry in range(len(prop_address)):
+    if entry == 0:
+        minor_address.send_keys(prop_address[entry])
+        minor_price.send_keys(prop_price[entry])
+        minor_links.send_keys(prop_links[entry])
+        minor_submit_clicker.click()
+
+
+    else:
+        major_address.send_keys(prop_address[entry])
+        major_price.send_keys(prop_price[entry])
+        major_links.send_keys(prop_links[entry])
+        major_submit_clicker.click()
+
+    if entry < len(prop_address) - 1:
+        time.sleep(3)
+        driver.find_element(By.CSS_SELECTOR, "body > div.Uc2NEf > div:nth-child(2) > div.RH5hzf.RLS9Fe > div > div.c2gzEf > a").click()
+        time.sleep(5)
