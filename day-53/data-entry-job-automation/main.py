cdc3b008cf129fdb9d122601e88009e1841eee25
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-10-02 16:25:18 +0530

    data entry automation complete

--- day-53/data-entry-job-automation/main.py
+++ day-53/data-entry-job-automation/main.py
@@ -35,16 +35,17 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(SHEETS_LINK)
 
-
 for x in range(LIMIT):
     time.sleep(3)
-    for c in driver.find_elements(By.CSS_SELECTOR, value=".Qr7Oae div div .AgroKb div div div input"):
-        c.send_keys("hello")
+    driver.find_element(By.XPATH, value='//input[@aria-describedby="i2 i3"]').send_keys(prop_address[x])
+    driver.find_element(By.XPATH, value='//input[@aria-describedby="i6 i7"]').send_keys(prop_price[x])
+    driver.find_element(By.XPATH, value='//input[@aria-describedby="i10 i11"]').send_keys(prop_links[x])
+
+    time.sleep(1)
+    s = driver.find_element(By.CSS_SELECTOR, value=".lRwqcd div")
+    s.click()
 
     if x < LIMIT - 1:
-        time.sleep(1)
-        s = driver.find_element(By.CSS_SELECTOR, value=".lRwqcd div")
-        s.click()
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, value=".c2gzEf a").click()
 
