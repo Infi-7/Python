477b47d09b4863df3db648b61b537fe8ae02c7c9
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-10-10 12:19:05 +0530

    flask-wtf form validation working

--- day-61/flask-wtf-forms/main.py
+++ day-61/flask-wtf-forms/main.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from tkinter.ttk import Button
 
 from dns.dnssec import validate
 from flask import *
@@ -23,33 +24,31 @@ This will install the packages from requirements.txt for this project.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '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'
 
-class MyForm(FlaskForm):
-    email = StringField('email', [DataRequired(), Email(), Length(min=5)])
-    password = StringField('password', [DataRequired(), Length(min=8)])
-    submit = SubmitField('Log in')
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(message='Email requirement not met'), Email(), Length(min=4)])
+    password = PasswordField('Password', validators=[DataRequired(message='Password requirement not met'), Length(min=8)])
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/login", methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    login_form = MyForm()
-    if login_form.validate_on_submit() and request.method == 'POST':
-        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
-            return render_template("success.html")
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+            return redirect('/success')
         else:
-            return render_template("denied.html")
+            return redirect('/denied')
     return render_template('login.html', form=login_form)
 
-@app.route("/success", methods=['GET','POST'])
+@app.route('/success')
 def success():
     return render_template('success.html')
 
-@app.route("/denied")
+@app.route('/denied')
 def denied():
-    return render_template("denied.html")
-
+    return render_template('denied.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
