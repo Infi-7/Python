965a1d6a86186307c8b1bd31340d5cef86d74ceb
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-10-12 15:56:58 +0530

    all database entries displaying on homepage update

---
+++ day-64/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from requests import session
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/infi/Documents/movies.db"
+db.init_app(app)
+
+# CREATE TABLE
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True)
+    year: Mapped[int] = mapped_column(Integer)
+    description: Mapped[str] = mapped_column(String)
+    rating: Mapped[float] = mapped_column(Float)
+    ranking: Mapped[int] = mapped_column(Integer)
+    review: Mapped[str] = mapped_column(String)
+    img_url: Mapped[str] = mapped_column(String)
+
+with app.app_context():
+    db.create_all()
+'''
+with app.app_context():
+    second_movie = Movie(
+        title="Avatar The Way of Water",
+        year=2022,
+        description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        rating=7.3,
+        ranking=9,
+        review="I liked the water.",
+        img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    )
+    db.session.add(second_movie)
+    db.session.commit()
+'''
+@app.route("/")
+def home():
+    with app.app_context():
+        multi_entry = db.session.execute(db.select(Movie))
+        result = multi_entry.scalars().all()
+        # entry = db.session.execute(db.select(Movie).where(Movie.title == "Phone Booth")).scalar()
+
+    return render_template("index.html", data=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
