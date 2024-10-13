9d811a2a4ef4b66ad1c1bf2bd76603efcba383e6
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-10-13 12:28:38 +0530

    full working update

--- day-64/main.py
+++ day-64/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from requests import session
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
@@ -13,61 +12,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
-# CREATE DB
+##CREATE DB
 class Base(DeclarativeBase):
     pass
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/infi/Documents/movies.db'
 db = SQLAlchemy(model_class=Base)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/infi/Documents/movies.db"
 db.init_app(app)
 
-# CREATE TABLE
 
+##CREATE TABLE
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String, unique=True)
-    year: Mapped[int] = mapped_column(Integer)
-    description: Mapped[str] = mapped_column(String)
-    rating: Mapped[float] = mapped_column(Float)
-    ranking: Mapped[int] = mapped_column(Integer)
-    review: Mapped[str] = mapped_column(String)
-    img_url: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 with app.app_context():
     db.create_all()
-
 
 class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5")
     review = StringField("Your Review")
     submit = SubmitField("Done")
 
-'''
-with app.app_context():
-    second_movie = Movie(
-        title="Avatar The Way of Water",
-        year=2022,
-        description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-        rating=7.3,
-        ranking=9,
-        review="I liked the water.",
-        img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-    )
-    db.session.add(second_movie)
-    db.session.commit()
-'''
 @app.route("/")
 def home():
-    with app.app_context():
-        multi_entry = db.session.execute(db.select(Movie))
-        result = multi_entry.scalars()
-        # entry = db.session.execute(db.select(Movie).where(Movie.title == "Phone Booth")).scalar()
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars()
+    return render_template("index.html", movies=all_movies)
 
-    return render_template("index.html", data=result)
-
-@app.route("/update", methods=["GET", "POST"])
-def update():
+# Adding the Update functionality
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
     form = RateMovieForm()
     movie_id = request.args.get("id")
     movie = db.get_or_404(Movie, movie_id)
@@ -77,7 +57,6 @@ def update():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
