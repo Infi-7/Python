c9ef91cf6b4b66a5a4b42bd66e89215e0d76f7c1
Infi-7 <aniketpatil2192000.bk@gmail.com>
2024-11-15 20:00:59 +0530

    index and resume pages complete

--- day-83/main.py
+++ day-83/main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
